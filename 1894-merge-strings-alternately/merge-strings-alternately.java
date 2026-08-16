a54215d5ef745f894995ba80eb2d4ddf1344c6fd
Ainaan Raza <razaainaan@gmail.com>
2026-08-17 00:29:25 +0530

    Time: 4 ms (19.31%) | Memory: 43.8 MB (16.16%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+        int j=0;
+        String s="";
+        while(i<word1.length() || j<word2.length()){
+            if(i>=word1.length()){
+                s+=word2.charAt(j);
+                j++;
+            }
+            else if(j>=word2.length()){
+                s+=word1.charAt(i);
+                i++;
+            }
+            else{
+                s+=word1.charAt(i);
+                s+=word2.charAt(j);
+                i++;
+                j++;
+            }
+        }
+        return s;
+    }
+}
